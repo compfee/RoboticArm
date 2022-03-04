1ff1d8e97894714a31a0d5c63389831b88ec42b0
Ane1y <9112539@mail.ru>
2022-03-05 01:13:47 +0300

    added full logic of stereovision module and some auxiliary files to work

--- source/stereovision/depth_map.py
+++ source/stereovision/depth_map.py
@@ -1,3 +1,5 @@
+import logging
+
 import cv2
 import imutils as imutils
 from matplotlib import pyplot as plt
@@ -6,33 +8,19 @@ import json
 from stereovision.calibration import StereoCalibrator
 from stereovision.calibration import StereoCalibration
 import params
+from image_handler import ImageHandler
 
-class DepthMap():
-    def __init__(self, image_path='../scenes/photo.png'):
-        self.imageToDisp = image_path
-        self.photo_width = params.PHOTO_WIDTH
-        self.photo_height = params.PHOTO_HEIGHT
-        self.params_file = '../src/pf_'+str(self.photo_width)+'_'+str(self.photo_height)+'.txt'
-        # Read parameters for image split
-        print ('Loading split parameters...')
-        data = json.load(open(self.params_file, 'r'))
-        self.imageWidth = data['imageWidth']
-        self.jointWidth = data['jointWidth']
-        self.leftIndent = data['leftIndent']
-        self.rightIndent = data['rightIndent']
-        self.image_size = (self.imageWidth - params.WIDTH_OFFSET, self.photo_height)
+logging.basicConfig(level='INFO')
+__metaclass__ = type
 
+class DepthMap(ImageHandler):
+    def __init__(self):
+        super(DepthMap, self).__init__()
 
-    def split_image(self):
-        print('Reading image rof depth map...')
-        pair_img = cv2.imread(self.imageToDisp,0)
-        imgLeft = pair_img [0:self.photo_height,self.leftIndent:self.imageWidth]
-        cv2.imwrite("../left_sample.jpg", imgLeft)#Y+H and X+W
-        imgRight = pair_img [0:self.photo_height,self.rightIndent:self.rightIndent+self.imageWidth-50]
-        cv2.imwrite("../right_sample.jpg", imgRight)
-        return imgLeft, imgRight
+    def split_image(self, imageToDisp):
+        return super(DepthMap, self).split_image(imageToDisp)
 
-    def plot(self, title, img, i):
+    def __plot(self, title, img, i):
         plt.subplot(2, 2, i)
         plt.title(title)
         plt.imshow(img, 'gray')
@@ -46,32 +34,33 @@ class DepthMap():
                               ndisparities=ndisp, SADWindowSize=sws)
         return stereo.compute(rectified_pair[0], rectified_pair[1])
 
-    def build_depth_map(self):
-        print('Load calibration data...')
+    def build_depth_map(self, image_path):
+        logging.info('Load calibration data...')
         calibration = StereoCalibration(input_folder='../ress')
-        rectified_pair = calibration.rectify((self.split_image()))
+        rectified_pair = calibration.rectify((self.split_image(image_path)))
         disparity = self.stereo_depth_map(rectified_pair, 256, 15)
-        print('Building depth map...')
+        logging.info('Building depth map...')
         return rectified_pair, disparity
 
-    def highlight_borders(self):
-        pass
+    def detect_contour(self, disparity):
         # ret, th = cv2.threshold(gray, 127, 255, 1)
         # kernel = np.ones((15, 15), np.uint8)
         # dilate = cv2.morphologyEx(th, cv2.MORPH_CLOSE, kernel, 3)
         # contours, hierarchy = cv2.findContours(dilate, 2, 1)
-        # cv2.drawContours(img, contours, -1, (0, 255, 0), 3)
+        # cv2.drawContours(self.plot(u'Depth map', disparity/255., 3), contours, -1, (0, 255, 0), 3)
+        pass
 
-    def draw_plot(self,rectified_pair, disparity):
-        norm_coeff = 255 / disparity.max()-disparity.min()
-        self.plot(u'Left calibrated', rectified_pair[0], 1)
-        self.plot(u'Right calibrated', rectified_pair[1], 2)
-        self.plot(u'Depth map', disparity/255., 3)
+    def draw_plot(self, rectified_pair, disparity):
+        self.__plot(u'Left calibrated', rectified_pair[0], 1)
+        self.__plot(u'Right calibrated', rectified_pair[1], 2)
+        self.__plot(u'Depth map', disparity/255., 3)
         plt.show()
-path_to_image = '../scenes/photo.png'
-depthMap = DepthMap(path_to_image)
-rectified_pair, disparity = depthMap.build_depth_map()
-print('Done! Let\'s look at depth map')
-print(disparity.max())
-depthMap.draw_plot(rectified_pair, disparity)
-depthMap.highlight_borders()
+
+depthMap = DepthMap()
+image = '../scenes/photo.png'
+try:
+    rectified_pair, disparity = depthMap.build_depth_map(image)
+    depthMap.draw_plot(rectified_pair, disparity)
+except:
+    logging.error("Wrong calibration directory")
+    # depthMap.highlight_borders()
