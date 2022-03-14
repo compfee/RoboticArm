d33adf72531f4747a5d0ada540db82b8d619dbf7
Somn117 <somn117@yandex.ru>
2022-03-14 17:45:52 +0300

    tests for preparation were renamed

--- source/test/rendering_testing_module/test_preparation.py
+++ source/test/rendering_testing_module/test_preparation.py
@@ -11,42 +11,36 @@ import shutil
 
 
 def test_preparation_with_existing_foulder():
-    try:
+
         primalFolderPath = str(get_project_root())+'/source/rendering_module/'
         assert preparation(primalFolderPath) == True
-    except:
-        print('ok')
+
 
 def test_preparation_without_existing_foulder():
-    try:
+
         primalFolderPath = str(get_project_root())+'/source/rendering_module/no_results'
         shutil.rmtree(primalFolderPath + str('/results'))
         assert preparation(primalFolderPath) == True
-    except:
-        print('ok')
+
 
 def test_check_dir_for_type_1():
-    try:
+
         primalFolderPath = str(get_project_root())+'/source/rendering_module/no_results'
         shutil.rmtree(primalFolderPath + str('/results'))
         cat = ("1", "2", "GBT", "3")
         assert check_dir(primalFolderPath, cat[0]) == False
-    except:
-        print('ok')
+
 
 def test_check_dir_for_type_gbt():
-    try:
+
         primalFolderPath = str(get_project_root())+'/source/rendering_module/no_results'
         shutil.rmtree(primalFolderPath + str('/results'))
         cat = ("1", "2", "GBT", "3")
         assert check_dir(primalFolderPath, cat[2]) == False
-    except:
-        print('ok')
+
 
 def test_check_dir_for_all_types():
-    try:
+
         primalFolderPath = str(get_project_root())+'/source/rendering_module/'
         cat = ("1", "2", "GBT", "3")
         assert check_dir(primalFolderPath, cat[2]) == True
-    except:
-        print('ok')
