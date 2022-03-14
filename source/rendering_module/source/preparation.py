e41acfa504af8ab9889cef214d5a88ee1cd73e1f
Somn117 <somn117@yandex.ru>
2022-03-14 17:10:21 +0300

    tests for preparation were added

--- source/rendering_module/source/preparation.py
+++ source/rendering_module/source/preparation.py
@@ -2,26 +2,21 @@ import os
 import sys
 
 
-primalFolderPath = 'C:/Users/Somn117/Documents/Blender-Auto-render-main'
+# primalFolderPath = 'C:/Users/Somn117/Documents/Blender-Auto-render-main'
 # primalFolderPath = input()
-
-if not os.path.isdir(primalFolderPath):
-    os.mkdir(primalFolderPath)
-
-categories = ("1", "2", "GBT", "3")
-
-for cat in categories:
+def check_dir(primalFolderPath, cat):
     try:
-        os.makedirs(primalFolderPath + "/results/" + cat + "/renderresult")
+        os.makedirs(primalFolderPath + "/results/" + cat)
+        return False
     except FileExistsError:
         print("Folder already exist")
-        
-    try:
-        os.makedirs(primalFolderPath + "/results/" + cat + "/renderresult_white")
-    except FileExistsError:
-        print("Folder already exist")
-        
-    try:
-        os.makedirs(primalFolderPath + "/results/" + cat + "/csv")
-    except FileExistsError:
-        print("Folder already exist")
+        return True
+
+def preparation(primalFolderPath):
+    if not os.path.isdir(primalFolderPath):
+        os.mkdir(primalFolderPath)
+    categories = ("1", "2", "GBT", "3")
+
+    for cat in categories:
+        check_dir(primalFolderPath, cat)
+    return True
