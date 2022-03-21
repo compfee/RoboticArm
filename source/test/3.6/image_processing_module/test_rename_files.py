17cf6210475f8b3603f4cf880854efc5ed418129
Somn117 <somn117@yandex.ru>
2022-03-22 01:33:07 +0300

    dir 3.6 added

---
+++ source/test/3.6/image_processing_module/test_rename_files.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+import os
+import pytest
+
+from source.image_processing_module.rename_function_module.rename import rename_files
+from source.utils import get_project_root
+
+
+def test_rename_files():
+    bool = True
+
+    rename_files()
+
+    images_path = str(get_project_root()) + "/source/image_processing_module/rename_function_module/pics"
+    image_list = os.listdir(images_path)
+    for i, image in enumerate(image_list):
+        bool &= image.endswith('nh' + str(i) + '.jpg')
+    assert bool == True
+
+
+def test_rename_files_ex():
+    with pytest.raises(Exception):
+        rename_files(directory=str(get_project_root()) + "/source/image_processing_module/rename_function_module")
---
+++ source/test/3.6/image_processing_module/test_utils.py
@@ -0,0 +1,6 @@
+from source.utils import get_project_root
+import pytest
+
+
+def test_utils():
+    assert str(get_project_root()).endswith('RoboticArm') == True
