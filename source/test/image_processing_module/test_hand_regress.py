8c930b4691ae1260bea41b68c2068e460925dde1
Somn117 <somn117@yandex.ru>
2022-03-22 01:01:22 +0300

    dir 3.6 added

--- source/test/image_processing_module/test_hand_regress.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from source.image_processing_module.hand_regress_module.hand_regress_module import GetDataSet
-
-
-def test_DATASET():
-    ds = GetDataSet()
-    ds.get_data_set()
-    assert (len(ds.train_coordi_dict) == 806) and (len(ds.test_coordi_dict) == 35)
--- source/test/image_processing_module/test_utils.py
+++
@@ -1,6 +0,0 @@
-from source.utils import get_project_root
-import pytest
-
-
-def test_utils():
-    assert str(get_project_root()).endswith('RoboticArm') == True
