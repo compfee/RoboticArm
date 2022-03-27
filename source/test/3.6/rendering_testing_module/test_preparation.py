5e83d47b521a05274ad7be9010263884c0dfd62b
Somn117 <somn117@yandex.ru>
2022-03-27 16:41:18 +0300

    inits were added

--- source/test/3.6/rendering_testing_module/test_preparation.py
+++ source/test/3.6/rendering_testing_module/test_preparation.py
@@ -4,7 +4,7 @@ import warnings
 import time
 import csv
 from tensorflow import keras
-from source.rendering_module.source.preparation import preparation, check_dir
+from rendering_module.source.preparation import preparation, check_dir
 from utils import get_project_root
 import os
 import shutil
