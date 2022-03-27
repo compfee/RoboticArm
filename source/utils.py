e2af0188b0ca2efa423f587ba25ac0697703ba49
Ane1y <9112539@mail.ru>
2022-03-27 21:12:52 +0300

    change test dir name

--- source/utils.py
+++ source/utils.py
@@ -6,4 +6,4 @@ def get_project_root():
     return Path(__file__).parent.parent
 
 def set_stereovision_dir():
-    os.chdir(str(get_project_root()) + '/source/stereovision_module')
+    os.chdir(str(get_project_root()) + '/source/stereovision_testing_module')
