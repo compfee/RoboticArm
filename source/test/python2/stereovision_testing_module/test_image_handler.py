46d57fef1b760f219b7d38bb144e9dd73c497fdc
Ane1y <9112539@mail.ru>
2022-03-27 21:39:49 +0300

    find the last error!

--- source/test/python2/stereovision_testing_module/test_image_handler.py
+++ source/test/python2/stereovision_testing_module/test_image_handler.py
@@ -3,7 +3,7 @@ import pytest
 from stereovision_module.image_handler import ImageHandler
 import os
 import cv2
-import stereovision_module.params
+import stereovision_module.params as params
 from utils import set_stereovision_dir
 
 set_stereovision_dir()
